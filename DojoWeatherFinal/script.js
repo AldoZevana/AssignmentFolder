c25ea5b97baacb31a98c4b127486e01867a927f7
AldoZevana <aldo.kurti.14@gmail.com>
2024-04-11 16:15:03 +0200

    test2

---
+++ DojoWeatherFinal/script.js
@@ -0,0 +1,19 @@
+function removeCookie(element){
+    element=document.getElementById("cookie-card")
+    element.remove();
+}
+
+function redSpan1(element1){
+    element1=document.getElementById("red-1").innerHTML()="75";
+}
+
+function redSpan2(element2){
+    element2=document.getElementById("red-2").innerHTML()="80";
+}
+
+function redSpan3(element3){
+    element3=document.getElementById("red-3").innerHTML()="69";
+}
+function redSpan4(element4){
+    element4=document.getElementById("red-4").innerHTML()="78";
+}
