7452e8027fd3d948f60d8c3090a08957aab8aae9
AldoZevana <aldo.kurti.14@gmail.com>
2024-04-11 18:29:05 +0200

    git commit -m "coded the problem"

--- DojoWeatherFinal/script.js
+++ DojoWeatherFinal/script.js
@@ -3,17 +3,39 @@ function removeCookie(element){
     element.remove();
 }
 
-function redSpan1(element1){
-    element1=document.getElementById("red-1").innerHTML()="75";
+function redSpan1(r_element1){
+    r_element1=document.getElementById("red-1").innerHTML="75";
+    console.log(r_element1);
 }
 
-function redSpan2(element2){
-    element2=document.getElementById("red-2").innerHTML()="80";
+function redSpan2(r_element2){
+    r_element2=document.getElementById("red-2").innerHTML="80";
+    console.log(r_element2);
 }
 
-function redSpan3(element3){
-    element3=document.getElementById("red-3").innerHTML()="69";
+function redSpan3(r_element3){
+    r_element3=document.getElementById("red-3").innerHTML="69";
+    console.log(r_element3);
 }
-function redSpan4(element4){
-    element4=document.getElementById("red-4").innerHTML()="78";
+function redSpan4(r_element4){
+    r_element4=document.getElementById("red-4").innerHTML="78";
+    console.log(r_element4);
+}
+function blueSpan1(b_element1){
+    b_element1=document.getElementById("blue-1").innerHTML="65";
+    console.log(b_element1);
+}
+
+function blueSpan2(b_element2){
+    b_element2=document.getElementById("blue-2").innerHTML="66";
+    console.log(b_element2);
+}
+
+function blueSpan3(b_element3){
+    b_element3=document.getElementById("blue-3").innerHTML="61";
+    console.log(b_element3);
+}
+function blueSpan4(b_element4){
+    b_element4=document.getElementById("blue-4").innerHTML="70";
+    console.log(b_element4);
 }
