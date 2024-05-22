428021c974f2cb1a84a7127e096bba63cf586aa9
AldoZevana <aldo.kurti.14@gmail.com>
2024-05-23 00:03:00 +0200

    Updated jokes.controller

--- MongoDB/Jokes_API/server/controllers/jokes.controller.js
+++ MongoDB/Jokes_API/server/controllers/jokes.controller.js
@@ -1,55 +1,40 @@
-require("../models/jokes.model")
+const Jokes = require('../models/jokes.model');
+
+// Find all jokes
 module.exports.findAllJokes = (req, res) => {
     Jokes.find()
-        .then((allDaJokes) => {
-            res.json({ joke: allDaJokes })
-        })
-        .catch((err) => {
-            res.json({ message: 'Something went wrong', error: err })
-        });
-}
+        .then(allDaJokes => res.json({ jokes: allDaJokes }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }));
+};
 
-
+// Find one single joke by ID
 module.exports.findOneSingleJoke = (req, res) => {
     Jokes.findOne({ _id: req.params.id })
-        .then(oneSingleJoke => {
-            res.json({ joke: oneSingleJoke })
-        })
-        .catch((err) => {
-            res.json({ message: 'Something went wrong', error: err })
-        });}
+        .then(oneSingleJoke => res.json({ joke: oneSingleJoke }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }));
+};
 
-        
-
+// Create a new joke
 module.exports.createNewJoke = (req, res) => {
     Jokes.create(req.body)
-        .then(newlyCreatedJoke => {
-            res.json({ jokes: newlyCreatedJoke })
-        })
-        .catch((err) => {
-            res.json({ message: 'Something went wrong', error: err })
-        });}
+        .then(newlyCreatedJoke => res.json({ joke: newlyCreatedJoke }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }));
+};
 
-
+// Update an existing joke by ID
 module.exports.updateExistingJoke = (req, res) => {
     Jokes.findOneAndUpdate(
         { _id: req.params.id },
         req.body,
         { new: true, runValidators: true }
     )
-        .then(updatedJoke => {
-            res.json({ joke: updatedJoke })
-        })
-        .catch((err) => {
-            res.json({ message: 'Something went wrong', error: err })
-        });}
+        .then(updatedJoke => res.json({ joke: updatedJoke }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }));
+};
 
-        
+// Delete an existing joke by ID
 module.exports.deleteAnExistingJoke = (req, res) => {
     Jokes.deleteOne({ _id: req.params.id })
-        .then(result => {
-            res.json({ result: result })
-        })
-        .catch((err) => {
-            res.json({ message: 'Something went wrong', error: err })
-        });}
+        .then(result => res.json({ result: result }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err }));
+};
